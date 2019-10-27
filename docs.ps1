cfd030466521151c3e22289ff9d6e449bcb4b419
Adrian Deller <adrian@deller.ch>
2019-10-27 15:41:38 +0100

    add external help

--- docs.ps1
+++ docs.ps1
@@ -6,13 +6,19 @@ Import-Module -Name platyPS
 $ModuleName = 'ADGroupMembershipMonitoring'
 
 Import-Module -Name .\$ModuleName -Force
-
 Get-Module -Name $ModuleName
-
 Get-Command -Module $ModuleName
 
-New-MarkdownHelp -Module $ModuleName -OutputFolder .\docs -NoMetadata -Force -WithModulePage
+# Create help
+New-MarkdownHelp -Module $ModuleName -OutputFolder .\docs -WithModulePage
+New-MarkdownHelp -Module $ModuleName -OutputFolder .\docs #-Force
 New-ExternalHelp -Path .\docs -OutputPath .\$ModuleName\en-US\
 
+# Update
+Update-MarkdownHelpModule -Path .\docs
+New-ExternalHelp -Path .\docs -OutputPath .\$ModuleName\en-US\ -Force
+
 # Testing Get-Help for module function
+Get-HelpPreview -Path .\$ModuleName\en-US\*.xml
 Get-Help Invoke-ADGroupMembershipMonitoring
+Get-Help Invoke-ADGroupMembershipMonitoring -Examples
