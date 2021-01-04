605304e822615832e5a319c12dd0d33a4d14a74d
Adrian Deller <adrian.deller@unibas.ch>
2021-01-04 20:50:07 +0100

    improve error handling in Get-ADObjectDetails

--- ADGroupMembershipMonitoring/private/Get-ADObjectDetails.ps1
+++ ADGroupMembershipMonitoring/private/Get-ADObjectDetails.ps1
@@ -25,13 +25,18 @@ function Get-ADObjectDetails
 
     Begin
     {
+        <#
+        # moved to Begin block in main function
         $DomainSidLookupTable = (Get-ADForest).Domains | Get-ADDomain | ForEach-Object -Begin { $ht = @{} } -Process {
             $ht[$PSItem.DomainSID.Value] = $PSItem
         } -End { return $ht }
+        #>
     }
     Process
     {
-        #Write-Verbose ("ParameterSetName: {0}" -f $PSCmdlet.ParameterSetName)
+        Write-Verbose ("ParameterSetName: {0}" -f $PSCmdlet.ParameterSetName)
+
+        $ADObjectToProcess = $null
 
         if ($PSCmdlet.ParameterSetName -eq 'ADObject')
         {
@@ -39,59 +44,66 @@ function Get-ADObjectDetails
             $DomainSid = $ADObject.SID.AccountDomainSid.Value
             $Server    = $DomainSidLookupTable[$DomainSid].DNSRoot
 
-            $SamAccountName = $ADObject.SamAccountName
-            $SID            = $ADObject.SID.Value
-            $ObjectClass    = $ADObject.ObjectClass
+            #$SamAccountName = $ADObject.SamAccountName
+            #$SID            = $ADObject.SID.Value
+            #$ObjectClass    = $ADObject.ObjectClass
+
+            $ADObjectToProcess = $ADObject
         }
         else
         {
             # collect required AD object properties (objectClass and SID) for further AD query
 
-            # Get Global Catalog DC
-            #$ServerGC = (Get-ADForest).GlobalCatalogs | Get-Random -Count 1
-            $ServerGC = (Get-ADDomainController -Discover -Service GlobalCatalog).Hostname
-            $Server = '{0}:3268' -f $ServerGC.Value
+            Write-Verbose "    Param 'SamAccountName': $SamAccountName"
+            Write-Verbose "    Param 'SID': $SID"
+            Write-Verbose "    Param 'ObjectClass': $ObjectClass"
 
             try
             {
-                if ([string]::IsNullOrEmpty($PSBoundParameters['ObjectClas']))
-                {
-                    $ADObjectResult = @(Get-ADObject -Filter "SamAccountName -eq '$SamAccountName'" -Server $Server -Property objectSid)
+                # Get Global Catalog DC
+                #$ServerGC = (Get-ADForest).GlobalCatalogs | Get-Random -Count 1
+                $ServerGC = (Get-ADDomainController -Discover -Service GlobalCatalog -ErrorAction Stop).Hostname
+                $Server = '{0}:3268' -f $ServerGC.Value
 
-                    Write-Verbose "SID: $SID"
+                #if ([string]::IsNullOrEmpty($PSBoundParameters['ObjectClass']))
+                #{
+                    $ADObjectResult = @(Get-ADObject -Filter "SamAccountName -eq '$SamAccountName'" -Server $Server -Property objectSid)
 
                     if ($ADObjectResult.Count -eq 1)
                     {
-                        $ObjectClass = $ADObjectResult.ObjectClass
-                        $SID         = $ADObjectResult.objectSid.Value
+                        $ADObjectToProcess = $ADObjectResult
+
+                        if ($ADObjectToProcess.ObjectClass)
+                        {
+                            Write-Verbose ("    Found AD object with ObjectClass '{0}'" -f $ADObjectToProcess.ObjectClass)
+                        }
                     }
                     elseif ($ADObjectResult.Count -gt 1)
                     {
                         $ADObjectResult = ($ADObjectResult | Where-Object { $_.objectSid.Value -eq $SID})
-                        $ObjectClass    = $ADObjectResult.ObjectClass
-                        $SID            = $ADObjectResult.objectSid.Value
+                        $ADObjectToProcess = $ADObjectResult
 
-                        if ($ObjectClass)
+                        if ($ADObjectToProcess.ObjectClass)
                         {
-                            Write-Verbose "Found AD object by SID '$SID'"
+                            Write-Verbose ("    Found AD object by SID '{0}'" -f $ADObjectToProcess.objectSid.Value)
                         }
                         else
                         {
-                            Write-Error "Found multiple AD objects with SamAccountName '$SamAccountName'"
-                            return
+                            Write-Warning "Found multiple AD objects with SamAccountName '$SamAccountName'"
                         }
                     }
 
-                    if ($ObjectClass)
+                    if (-not ($ADObjectToProcess.ObjectClass))
                     {
-                        Write-Verbose "Found AD object with ObjectClass '$ObjectClass'"
+                        $PSCmdlet.WriteError([System.Management.Automation.ErrorRecord]::new(
+                            "Could not determine ObjectClass of '$SamAccountName'", # exception
+                            $null, # errorId
+                            [System.Management.Automation.ErrorCategory]::NotSpecified, # errorCategory
+                            $SamAccountName) # targetObject
+                          )
+                          return
                     }
-                    else
-                    {
-                        Write-Error "Unable to identify ObjectClass of '$SamAccountName'"
-                        return
-                    }
-                }
+                #}
             }
             catch
             {
@@ -99,13 +111,22 @@ function Get-ADObjectDetails
             }
         }
 
+        if ($PSCmdlet.ParameterSetName -eq 'ADObject')
+        {
+            $Filter = "SID -eq '{0}'" -f $ADObjectToProcess.SID.Value
+        }
+        else
+        {
+            $Filter = "SID -eq '{0}'" -f $ADObjectToProcess.objectSid.Value
+        }
+
         $ADObjectSplatting = @{
-            Filter      = "SID -eq '$SID'"
+            Filter      = $Filter
             Server      = $Server
             ErrorAction = 'SilentlyContinue'
         }
 
-        $ADObjectReturn = switch ($ObjectClass)
+        $ADObjectReturn = switch ($ADObjectToProcess.ObjectClass)
         {
             'computer' { Get-ADComputer @ADObjectSplatting }
             'group' { Get-ADGroup @ADObjectSplatting }
@@ -119,21 +140,19 @@ function Get-ADObjectDetails
         }
         else
         {
-            #throw "AD object '$SamAccountName' not found"
-
-            #
             if ($PSBoundParameters['ErrorAction'] -eq 'Stop')
             {
-                throw "AD object '$SamAccountName' not found"
+                throw "Could not find AD object '$SamAccountName'"
             }
             else
             {
-                return [PSCustomObject]@{
-                    SamAccountName = $SamAccountName
-                    #ObjectClass    = $ObjectClass
-                }
+                $PSCmdlet.WriteError([System.Management.Automation.ErrorRecord]::new(
+                    "Could not find AD object '$SamAccountName'",
+                    $null, # error ID
+                    [System.Management.Automation.ErrorCategory]::InvalidData, # error category
+                    $SamAccountName) # offending object
+                  )
             }
-            #
         }
     }
     End
